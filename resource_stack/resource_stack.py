8048897fd787262e2d1b15872829685518abd6a0
viv327 <jingzhang@jings-mbp.mynetworksettings.com>
2023-08-14 20:16:03 -0400

    Add bundling option to include external dependency such as Pydantic

--- resource_stack/resource_stack.py
+++ resource_stack/resource_stack.py
@@ -1,8 +1,9 @@
 from aws_cdk import (
+    BundlingOptions,
     Stack,
-    aws_lambda as function_lambda,
-    aws_s3 as s3,
-    aws_dynamodb as dynamodb
+    aws_lambda,
+    aws_s3,
+    aws_dynamodb
 )
 from constructs import Construct
 
@@ -12,44 +13,53 @@ class ResourceStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # main service Lambda
-        _lambda_layer = function_lambda.LayerVersion(
+        _lambda_layer = aws_lambda.LayerVersion(
             self, "Boto3LambdaLayer",
-            code=function_lambda.Code.from_asset('./lambda_code_asset/busy_baby'),
-            compatible_runtimes=[function_lambda.Runtime.PYTHON_3_9]
+            code=aws_lambda.Code.from_asset('./lambda_code_asset/busy_baby'),
+            compatible_runtimes=[aws_lambda.Runtime.PYTHON_3_9]
         )
 
-        function = function_lambda.Function(self,
-                                            "SparcBusyBabyService",
-                                            function_name="SparcBusyBabyService",
-                                            runtime=function_lambda.Runtime.PYTHON_3_9,
-                                            layers=[_lambda_layer],
-                                            code=function_lambda.Code.from_asset('./lambda_code_asset'),
-                                            handler="service_lambda.main")
+        function = aws_lambda.Function(self,
+                                       "SparcBusyBabyService",
+                                       function_name="SparcBusyBabyService",
+                                       runtime=aws_lambda.Runtime.PYTHON_3_9,
+                                       layers=[_lambda_layer],
+                                       code=aws_lambda.Code.from_asset(
+                                           './lambda_code_asset',
+                                           bundling=BundlingOptions(
+                                               image=aws_lambda.Runtime.PYTHON_3_9.bundling_image,
+                                               command=[
+                                                   "bash", "-c",
+                                                   "pip install --no-cache -r requirements.txt -t /asset-output && cp -au . /asset-output"
+                                               ],
+                                           )
+                                       ),
+                                       handler="service_lambda.main")
 
         # S3 bucket
-        bucket = s3.Bucket(self, "SparcBusyBabyBucket", versioned=True,
-                           bucket_name="demo-bucket-for-sparc-aws-code-deploy",
-                           block_public_access=s3.BlockPublicAccess.BLOCK_ALL)
+        bucket = aws_s3.Bucket(self, "SparcBusyBabyBucket", versioned=True,
+                               bucket_name="demo-bucket-for-sparc-aws-code-deploy",
+                               block_public_access=aws_s3.BlockPublicAccess.BLOCK_ALL)
 
         # DynamoDB
-        daily_record_table = dynamodb.Table(self, "SparcBusyBabyDailyRecord",
-                                            table_name="SparcBusyBabyDailyRecord",
-                                            partition_key=dynamodb.Attribute(
-                                                name="baby_id",
-                                                type=dynamodb.AttributeType.STRING
-                                            ),
-                                            sort_key=dynamodb.Attribute(
-                                                name="record_date",
-                                                type=dynamodb.AttributeType.STRING
-                                            )
-                                            )
+        daily_record_table = aws_dynamodb.Table(self, "SparcBusyBabyDailyRecord",
+                                                table_name="SparcBusyBabyDailyRecord",
+                                                partition_key=aws_dynamodb.Attribute(
+                                                    name="baby_id",
+                                                    type=aws_dynamodb.AttributeType.STRING
+                                                ),
+                                                sort_key=aws_dynamodb.Attribute(
+                                                    name="record_date",
+                                                    type=aws_dynamodb.AttributeType.STRING
+                                                )
+                                                )
         daily_record_table.grant_read_write_data(function.role)
 
-        baby_profile_table = dynamodb.Table(self, "SparcBusyBabyProfile",
-                                            table_name="SparcBusyBabyProfile",
-                                            partition_key=dynamodb.Attribute(
-                                                name="baby_id",
-                                                type=dynamodb.AttributeType.STRING
-                                            )
-                                            )
+        baby_profile_table = aws_dynamodb.Table(self, "SparcBusyBabyProfile",
+                                                table_name="SparcBusyBabyProfile",
+                                                partition_key=aws_dynamodb.Attribute(
+                                                    name="baby_id",
+                                                    type=aws_dynamodb.AttributeType.STRING
+                                                )
+                                                )
         baby_profile_table.grant_read_write_data(function.role)

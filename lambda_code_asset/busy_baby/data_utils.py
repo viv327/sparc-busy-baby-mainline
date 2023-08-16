da7fb50ad06caf1a07f2f296e263b261111630bf
viv <jingzhang@jings-mbp.mynetworksettings.com>
2023-08-16 14:24:12 -0400

    test addGrowthRecord initial

--- lambda_code_asset/busy_baby/data_utils.py
+++ lambda_code_asset/busy_baby/data_utils.py
@@ -1,14 +1,18 @@
 import boto3
+import logging
 import os
 import uuid
 from .constants import BABY_PROFILE_DDB_TABLE, DAILY_RECORD_DDB_TABLE, DEMO_BABY_ID
-from .models.basic_info import BabyProfile
+from .models.basic_info import Growth, BabyProfile
 from .models.daily_record import DailyRecord
-from .persistance.ddb_item import BabyProfileDDBItem, DailyRecordDDBItem
+from .persistance.ddb_item import BabyProfileDDBItemAttrs, BabyProfileDDBItem, DailyRecordDDBItemAttrs, DailyRecordDDBItem
 
 dynamodb = boto3.resource("dynamodb")
 baby_profile_table = dynamodb.Table(BABY_PROFILE_DDB_TABLE)
 daily_record_table = dynamodb.Table(DAILY_RECORD_DDB_TABLE)
+
+
+logger = logging.getLogger(__name__)
 
 
 def create_baby(first_name, last_name, gender, birthday):
@@ -32,3 +36,32 @@ def create_baby(first_name, last_name, gender, birthday):
 
     # return "(calculated result based on DDB query, e.g,  milk intake volume)"
     return "Success"
+
+
+def add_growth_record(baby_id, record_datetime, height, weight, head_circumference):
+    try:
+        growth_record = Growth(
+            record_datetime=record_datetime,
+            height=height,
+            weight=weight,
+            head_circumference=head_circumference
+        )
+        update_expression = f"set {BabyProfileDDBItemAttrs.GROWTH_RECORD.ddb_attr}"
+        expression_attr_values = {
+            ':baby_id': baby_id,
+            ':growth_record': [growth_record.dict()]
+        }
+
+        baby_profile_table.update_item(
+            Key={
+                BabyProfileDDBItemAttrs.BABY_ID.ddb_attr: baby_id
+            },
+            UpdateExpression=update_expression,
+            ConditionExpression="baby_id = :baby_id",
+            ExpressionAttributeValues=expression_attr_values
+        )
+
+        return "Success"
+    except Exception as e:
+        logger.error("Failed to update baby profile")
+        raise e
--- lambda_code_asset/service_lambda.py
+++ lambda_code_asset/service_lambda.py
@@ -1,6 +1,7 @@
 import json
-
-from busy_baby.data_utils import create_baby
+from datetime import datetime
+from busy_baby.data_utils import create_baby, add_growth_record
+from busy_baby.constants import DEMO_BABY_ID
 
 
 def dispatch(intent: str, slots: any):
@@ -17,8 +18,27 @@ def dispatch(intent: str, slots: any):
         result = create_baby(first_name, last_name, gender, birthday)
         message = "Baby profile creation result: {}".format(result)
 
-    if intent == "createGrowthRecord":
-        result = {}
+    if intent == "addGrowthRecord":
+        record_datetime = datetime.utcnow()
+
+        if "Height" in slots and "value" in slots["Height"]:
+            height = slots["Height"]["value"]["interpretedValue"]
+        else:
+            height = None
+
+        if "Weight" in slots and "value" in slots["Weight"]:
+            weight = slots["Weight"]["value"]["interpretedValue"]
+        else:
+            weight = None
+
+        if "HeadCircumference" in slots and "value" in slots["HeadCircumference"]:
+            head_circumference = slots["HeadCircumference"]["value"]["interpretedValue"]
+        else:
+            head_circumference = None
+
+        result = add_growth_record(DEMO_BABY_ID, record_datetime, height, weight, head_circumference)
+        message = "Update baby growth record result: {}".format(result)
+
     if intent == "createVaccineRecord":
         result = {}
 

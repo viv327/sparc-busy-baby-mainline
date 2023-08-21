b8cc0315799108646193252aa91e3e42fb7b8f96
Sheilalulalei <91455769+Sheilalulalei@users.noreply.github.com>
2023-08-21 12:08:44 -0700

    text to speech test

--- lambda_code_asset/service_lambda.py
+++ lambda_code_asset/service_lambda.py
@@ -3,6 +3,13 @@ from datetime import datetime
 
 import dateutil.tz
 import boto3
+from boto3 import Session
+from botocore.exceptions import BotoCoreError, ClientError
+from contextlib import closing
+import os
+import sys
+import subprocess
+from tempfile import gettempdir
 
 from busy_baby.api import create_baby, add_growth_record, add_vaccine_record, add_bottle_feed, add_sleep_record, \
     add_nurse_feed, add_solid_food, add_diaper_pee, add_diaper_poo, add_bath, add_medicine, get_most_recent_height, \
@@ -384,6 +391,7 @@ def dispatch(intent: str, slots: any):
         ]
     }
 
+
     '''
     - createBaby(first_name, last_name, gender, birthday)x
     - addGrowthRecord(baby_id, record_date, height, weight, head_circumference)
@@ -431,6 +439,8 @@ def dispatch(intent: str, slots: any):
 
     '''
     # TODO: add more intents here, each with a handling function
+
+    textToSpeech(message)
     return response
 
 
@@ -446,7 +456,51 @@ def delegate(intent: str, slots: any):
             }
         }
     }
+def textToSpeech(message):
+    # Create a client using the credentials and region defined in the [adminuser]
+    # section of the AWS credentials file (~/.aws/credentials).
+    session = Session(profile_name="adminuser")
+    polly = session.client("polly")
 
+    try:
+        # Request speech synthesis
+        response = polly.synthesize_speech(Text=message, OutputFormat="mp3",
+                                            VoiceId="Joanna")
+    except (BotoCoreError, ClientError) as error:
+        # The service returned an error, exit gracefully
+        print(error)
+        sys.exit(-1)
+
+    # Access the audio stream from the response
+    if "AudioStream" in response:
+        # Note: Closing the stream is important because the service throttles on the
+        # number of parallel connections. Here we are using contextlib.closing to
+        # ensure the close method of the stream object will be called automatically
+        # at the end of the with statement's scope.
+            with closing(response["AudioStream"]) as stream:
+               output = os.path.join(gettempdir(), "speech.mp3")
+
+               try:
+                # Open a file for writing the output as a binary stream
+                    with open(output, "wb") as file:
+                       file.write(stream.read())
+               except IOError as error:
+                  # Could not write to file, exit gracefully
+                  print(error)
+                  sys.exit(-1)
+
+    else:
+        # The response didn't contain audio data, exit gracefully
+        print("Could not stream audio")
+        sys.exit(-1)
+    # p
+    # Play the audio using the platform's default player
+    if sys.platform == "win32":
+        os.startfile(output)
+    else:
+        # The following works on macOS and Linux. (Darwin = mac, xdg-open = linux).
+        opener = "open" if sys.platform == "darwin" else "xdg-open"
+        subprocess.call([opener, output])
 
 def main(event, context):
     print('request: {}'.format(json.dumps(event)))
